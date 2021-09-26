f204c97ffb437e2cd371fa47b71f976edd18a4b0
Adam <adamskoullos@gmail.com>
2021-09-26 13:42:59 +0100

    comment out middleware logic and test deployment again

--- middleware/redirects.js
+++ middleware/redirects.js
@@ -1,10 +1,10 @@
 export default ({ route, redirect, req, res }) => {
   if (process.server) {
     // For larger projects server logic could be placed in specific serverMiddleware that only runs on the server to improve performance
-    if (route.path == "/") {
-      res.writeHead(301, { Location: "/list-one" });
-      res.end();
-    }
+    // if (route.path == "/") {
+    //   res.writeHead(301, { Location: "/list-one" });
+    //   res.end();
+    // }
   }
 
   if (process.client) {
